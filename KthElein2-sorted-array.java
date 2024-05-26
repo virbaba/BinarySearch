c18d2babb988eaa20444bebbf67605036f74b17e
Virender Singh <virendersinghdev1@gmail.com>
2024-05-26 15:27:36 +0530

    Update KthElein2-sorted-array.java

--- KthElein2-sorted-array.java
+++ KthElein2-sorted-array.java
@@ -1,41 +1,35 @@
 import java.util.ArrayList;
+
 public class Solution {
     public static int kthElement(ArrayList<Integer> arr1, ArrayList<Integer> arr2, int n, int m, int k) {
         // Write your coder here
-         if (n > m) {
-            // Swap arrays and their sizes to ensure arr1 is smaller
-            ArrayList<Integer> tempArr = arr1;
-            arr1 = arr2;
-            arr2 = tempArr;
 
-            int tempN = n;
-            n = m;
-            m = tempN;
-        }
-
+        // how much min element need in first array
         int low = Math.max(0, k - m);
+        // how much max element need in first array
         int high = Math.min(n, k);
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int arr2Index = k - mid;
+            int aleft = low + (high - low) / 2;
+            // k - aleft mean total need k and we took aleft size element from first array
+            int bleft = k - aleft;
 
-            int leftMaxArr1 = (mid == 0) ? Integer.MIN_VALUE : arr1.get(mid - 1);
-            int rightMinArr1 = (mid == n) ? Integer.MAX_VALUE : arr1.get(mid);
+            int aMax = (aleft == 0) ? Integer.MIN_VALUE : arr1.get(aleft - 1);
+            int aMin = (aleft == n) ? Integer.MAX_VALUE : arr1.get(aleft);
 
-            int leftMaxArr2 = (arr2Index == 0) ? Integer.MIN_VALUE : arr2.get(arr2Index - 1);
-            int rightMinArr2 = (arr2Index == m) ? Integer.MAX_VALUE : arr2.get(arr2Index);
+            int bMax = (bleft == 0) ? Integer.MIN_VALUE : arr2.get(bleft - 1);
+            int bMin = (bleft == m) ? Integer.MAX_VALUE : arr2.get(bleft);
 
-            if (leftMaxArr1 <= rightMinArr2 && leftMaxArr2 <= rightMinArr1) {
-                return Math.max(leftMaxArr1, leftMaxArr2);
-            } else if (leftMaxArr1 > rightMinArr2) {
-                high = mid - 1;
+            if (aMax <= bMin && bMax <= aMin) {
+                return Math.max(aMax, bMax);
+            } else if (aMax > bMin) {
+                high = aleft - 1;
             } else {
-                low = mid + 1;
+                low = aleft + 1;
             }
         }
 
         throw new IllegalArgumentException("Invalid value of k.");
-        
+
     }
 }
