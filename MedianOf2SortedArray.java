7dba037cdfe7e8c0b23f6184e23ad22cd2329792
Virender Singh <virendersinghdev1@gmail.com>
2024-05-26 15:26:58 +0530

    Update MedianOf2SortedArray.java

--- MedianOf2SortedArray.java
+++ MedianOf2SortedArray.java
@@ -1,46 +1,35 @@
-import java.util.*;
-
 public class Solution {
     public static double median(int[] a, int[] b) {
         // Write your code here.
+
         int n1 = a.length;
         int n2 = b.length;
-
-        if (n1 > n2) {
-            int[] temp = a;
-            a = b;
-            b = temp;
-
-            int tempN = n1;
-            n1 = n2;
-            n2 = tempN;
-        }
+        int te = n1 + n2;
 
         int s = 0, e = n1;
 
         while (s <= e) {
-            int mid = s + (e - s) / 2;
-            int b1 = (n1 + n2 + 1) / 2 - mid;
+            int aleft = (s + e) / 2;
+            int bleft = (te + 1) / 2 - aleft;
 
-            int leftMaxA = (mid == 0) ? Integer.MIN_VALUE : a[mid - 1];
-            int rightMinA = (mid == n1) ? Integer.MAX_VALUE : a[mid];
+            int amax = (aleft == 0) ? Integer.MIN_VALUE : a[aleft - 1];
+            int amin = (aleft == n1) ? Integer.MAX_VALUE : a[aleft];
 
-            int leftMaxB = (b1 == 0) ? Integer.MIN_VALUE : b[b1 - 1];
-            int rightMinB = (b1 == n2) ? Integer.MAX_VALUE : b[b1];
+            int bmax = (bleft == 0) ? Integer.MIN_VALUE : b[bleft - 1];
+            int bmin = (bleft == n2) ? Integer.MAX_VALUE : b[bleft];
 
-            if (leftMaxA <= rightMinB && leftMaxB <= rightMinA) {
-                if ((n1 + n2) % 2 == 0) {
-                    return (Math.max(leftMaxA, leftMaxB) + Math.min(rightMinA, rightMinB)) / 2.0;
+            if (amax <= bmin && bmax <= amin) {
+                if (te % 2 == 0) {
+                    return (Math.max(amax, bmax) + Math.min(amin, bmin)) / 2.0;
                 } else {
-                    return Math.max(leftMaxA, leftMaxB);
+                    return Math.max(amax, bmax) / 1.0;
                 }
-            } else if (leftMaxA > rightMinB) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
+            } else if (amax > bmin)
+                e = aleft - 1;
+            else
+                s = aleft + 1;
         }
 
-        throw new IllegalArgumentException("Input arrays are not sorted.");
+        return 0.0;
     }
 }
